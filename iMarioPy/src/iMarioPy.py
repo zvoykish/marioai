3ba833eb1cd1553be77258e0a4a541095d82d9b5
Sergey.Karakovskiy <Sergey.Karakovskiy@91117294-33d9-11de-9c63-4752e237b823>
2009-04-29 23:31:45 +0000

    Simple TCP Agent improvements

--- iMarioPy/src/iMarioPy.py
+++ iMarioPy/src/iMarioPy.py
@@ -3,12 +3,24 @@ __author__="Duke"
 __date__ ="$Apr 29, 2009 9:06:11 PM$"
 
 import numpy
-import newModule
+import TCP
 import python
 
 
 if __name__ == "__main__":
     print numpy.random.randint(34, 39)
     print "Hello";
-    print newModule.hi()
-    print python.HOST
+    print TCP.__name__
+    print TCP.__author__
+    print python.__author__
+
+
+if __name__ == "__main__":
+    print "Hello World";
+else:
+    print "SimpleAgent:"
+
+HOST = 'localhost'
+PORT = 4242;
+
+sys.exit(0)
