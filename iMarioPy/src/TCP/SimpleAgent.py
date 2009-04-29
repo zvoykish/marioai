588c2ea2467c9c2a58ab010c8ff269549df0d33c
Sergey.Karakovskiy <Sergey.Karakovskiy@91117294-33d9-11de-9c63-4752e237b823>
2009-04-29 20:10:38 +0000

    TCP Agent

---
+++ iMarioPy/src/TCP/SimpleAgent.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+__author__="Duke"
+__date__ ="$Apr 29, 2009 10:11:15 PM$"
+
+if __name__ == "__main__":
+    print "Hello World";
---
+++ iMarioPy/src/iMarioPy.py
@@ -0,0 +1,14 @@
+
+__author__="Duke"
+__date__ ="$Apr 29, 2009 9:06:11 PM$"
+
+import numpy
+import newModule
+import python
+
+
+if __name__ == "__main__":
+    print numpy.random.randint(34, 39)
+    print "Hello";
+    print newModule.hi()
+    print python.HOST
