ed679a1e06132d8b33e4692a1a76ea07e9a91fea
Sergey.Karakovskiy <Sergey.Karakovskiy@91117294-33d9-11de-9c63-4752e237b823>
2010-03-17 07:44:24 +0000

    Championship Evaluation components added

---
+++ src/ch/idsia/scenarios/Main.java
@@ -0,0 +1,32 @@
+package ch.idsia.scenarios;
+
+import ch.idsia.ai.agents.Agent;
+import ch.idsia.ai.agents.ai.ForwardAgent;
+import ch.idsia.ai.agents.human.HumanKeyboardAgent;
+import ch.idsia.maibe.tasks.BasicTask;
+import ch.idsia.mario.environments.Environment;
+import ch.idsia.mario.environments.MarioEnvironment;
+import ch.idsia.tools.CmdLineOptions;
+import ch.idsia.tools.EvaluationInfo;
+
+/**
+ * Created by IntelliJ IDEA. User: Sergey Karakovskiy, sergey at idsia dot ch Date: Mar 17, 2010 Time: 8:28:00 AM
+ * Package: ch.idsia.scenarios
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String argsString = "-vis on";
+        args = argsString.split("\\s");
+        final CmdLineOptions cmdLineOptions = new CmdLineOptions(args);
+        final Environment environment = new MarioEnvironment();
+        final Agent agent = new ForwardAgent();
+        final BasicTask basicTask = new BasicTask(environment, agent);
+        basicTask.reset(cmdLineOptions);
+        basicTask.runEpisode();
+        EvaluationInfo evaluationInfo = new EvaluationInfo(environment.getEvaluationInfo());
+        System.out.println("evaluationInfo = " + evaluationInfo);
+        System.exit(0);
+    }
+}
