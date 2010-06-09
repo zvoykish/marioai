6c465629239b16763fed71a182283e578260ad2c
sergey.karakovskiy@gmail.com <sergey.karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-06-09 17:05:44 +0000

    MarioAI update to version 0.1.6 Level Generator update: new features include customizable: {hidden block, dead-ends, level height, creatures, coins, tubes(flower pots), cannons} So, your agent can incrementally learn the increasing difficulty of environment. LearningEvaluation class updated

---
+++ src/ch/idsia/ai/agents/BasicLearningAgent.java
@@ -0,0 +1,26 @@
+package ch.idsia.ai.agents;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sergey Karakovskiy, sergey.karakovskiy@gmail.com
+ * Date: Jun 9, 2010
+ * Time: 7:03:17 PM
+ * Package: ch.idsia.ai.agents
+ */
+public class BasicLearningAgent implements LearningAgent extends SimpleMLPAgent
+{
+    public void learn()
+    {
+
+    }
+
+    public void giveReward()
+    {
+
+    }
+
+    public void newEpisode()
+    {
+        
+    }
+}
