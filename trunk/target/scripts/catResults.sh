a5698ebd99e02ca342cf1ff4db3ad6baa97ddb61
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-08-31 23:36:10 +0000

    target/scripts updated, gic2010 package added

--- trunk/target/scripts/catResults.sh
+++ trunk/target/scripts/catResults.sh
@@ -5,8 +5,8 @@ LIM=6
 OUT=MarioAIResult.m
 while [ $I -le $LIM ]
 do
-	echo	processing iMario${I}.m ...
-	cat MarioAI${I}.m >> ${OUT}
-	I=$(($I+1))
+    echo processing iMario${I}.m ...
+    cat MarioAI${I}.m >> ${OUT}
+    I=$(( $I + 1 ))
 done
 echo "results saved to ${OUT}" 
