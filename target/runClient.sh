667945d356f64c73af7c4adc1b34d7340ee2ab80
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-08-31 23:33:13 +0000

    target/scripts added

--- target/runClient.sh
+++ target/runClient.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+
 I=1
 LIM=2
 PORT=4242
 while [ $I -le $LIM ]
 do
-	echo $I $PORT
-	python ../src/Python/iPyMario/src/iPyMario.py --agent ForwardAgent --port ${PORT} &
-	PORT=$(($PORT+1))
-	I=$(($I+1))
+    echo $I $PORT
+    python marioai.py --agent ForwardAgent --port ${PORT} &
+    PORT=$(( $PORT + 1 ))
+    I=$(( $I + 1 ))
 done
-python ../src/Python/iPyMario/src/iPyMario.py --agent ForwardRandomAgent --port ${PORT} &
+python marioai.py --agent ForwardRandomAgent --port ${PORT} &
