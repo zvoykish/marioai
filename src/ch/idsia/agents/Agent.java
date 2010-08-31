859abd28cc90e68d6c028b04c0fa3b07b17d54c8
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-08-31 23:00:05 +0000

    refactoring complete

---
+++ src/ch/idsia/agents/Agent.java
@@ -0,0 +1,35 @@
+package ch.idsia.agents;
+
+import ch.idsia.mario.environments.Environment;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sergey Karakovskiy
+ * Date: Mar 28, 2009
+ * Time: 8:46:42 PM
+ * package ch.idsia.controllers.agents;
+ */
+public interface Agent
+{
+    boolean[] getAction();
+
+    void integrateObservation(Environment environment);
+
+    void giveIntermediateReward(float intermediateReward);
+
+    public enum AGENT_TYPE
+    {
+        AI, HUMAN, TCP_SERVER
+    }
+
+    // clears all dynamic data, such as hidden layers in recurrent networks
+    // just implement an empty method for a reactive controller
+
+    public void reset();
+
+    public AGENT_TYPE getType();
+
+    public String getName();
+
+    public void setName(String name);
+}
