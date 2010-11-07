2a779d9a847d4d7b04c086539eabf7ad53a08c28
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-11-07 22:54:47 +0000

    fixes for Mac OS X scripts

--- src/amico/python/PyJava/run.sh
+++ src/amico/python/PyJava/run.sh
@@ -19,43 +19,48 @@ LIBRARY_FILE_NAME=
 COMPILE_LIBRARY="true"
 
 if [ "$#" -gt 0 ]; then
-	until [ -z "$1"  ];
-	do
-		case "$1" in
-		"-nc"	) COMPILE_LIBRARY="false";;
-		"-ch"	) shift; MARIO_DIR="$1";;
-		"-out"	) shift; OUT_DIR="$1";;
-		"-o"	) shift; CMD_LINE_OPTIONS="$1";;
-		"-agent") shift; AGENT="$1";;
-		*	) echo "Usage: [-ch path_to_ch_directory] [-out path_to_output_directory]"; exit 1;;
-		esac
-		shift
-	done
+    until [ -z "$1" ];
+    do
+        case "$1" in
+            "-nc") COMPILE_LIBRARY="false" ;;
+            "-ch") shift;
+            MARIO_DIR="$1" ;;
+            "-out") shift;
+            OUT_DIR="$1" ;;
+            "-o") shift;
+            CMD_LINE_OPTIONS="$1" ;;
+            "-ag") shift;
+            AGENT="$1" ;;
+            *) echo "Usage: [-ch path_to_ch_directory] [-out path_to_output_directory]";
+            exit 1 ;;
+        esac
+        shift
+    done
 fi
 
 if [ "$COMPILE_LIBRARY" = "true" ]; then
-	make
+    make
 fi
 
 mkdir -p "$OUT_DIR/$BUILD_DIR"
 
-if test "${OS}" = ""; then
-	OS=`uname -s` # Retrieve the Operating System
-fi
+#if test "${OS}" = ""; then
+OS=`uname -s` # Retrieve the Operating System
+#fi
 if [ "${OS}" = "Linux" ]; then
-	LIBRARY_FILE_NAME="libAmiCoPyJava.so"
+    LIBRARY_FILE_NAME="libAmiCoPyJava.so"
 elif [ "${OS}" = "Darwin" ]; then
-	LIBRARY_FILE_NAME="libAmiCoPyJava.dylib"
+    LIBRARY_FILE_NAME="libAmiCoPyJava.dylib"
 fi
 
 if [ "$LIBRARY_FILE_NAME" = "" ]; then
-	echo "Unknown OS type: ${OS}. Exit..."
-	exit 1
+    echo "Unknown OS type: ${OS}. Exit..."
+    exit 1
 fi
 
 if [ "$COMPILE_LIBRARY" = "true" ]; then
-	mv "$MAKE_OUT_DIR/$LIBRARY_FILE_NAME" "$OUT_DIR/$BUILD_DIR/"
-	rm -rf "$MAKE_OUT_DIR"
+    mv "$MAKE_OUT_DIR/$LIBRARY_FILE_NAME" "$OUT_DIR/$BUILD_DIR/"
+    rm -rf "$MAKE_OUT_DIR"
 fi
 
 cp AmiCoRunner.sh "$OUT_DIR/$BUILD_DIR/"
@@ -63,12 +68,12 @@ cp ../agents/*.py "$OUT_DIR/$BUILD_DIR/"
 cp -r "$MARIO_DIR/ch" "$OUT_DIR/$BUILD_DIR/"
 
 if [ "$AGENT" = "" ]; then
-	echo "Incorrect input! Option '-agent <agent_file>' is necessary!"
+    echo "Incorrect input! Option '-agent <agent_file>' is necessary!"
 fi
 
 cd "$OUT_DIR/$BUILD_DIR/"
 if [ "${OS}" = "Linux" ]; then
-	./AmiCoRunner.sh "$AGENT" "$CMD_LINE_OPTIONS"
+    ./AmiCoRunner.sh "$AGENT" "$CMD_LINE_OPTIONS"
 elif [ "${OS}" = "Darwin" ]; then
-	python "$AGENT" "$CMD_LINE_OPTIONS"
+    python "$AGENT" "$CMD_LINE_OPTIONS"
 fi
