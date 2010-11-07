2464a59b1ba9b4fbecaa7e50f7fa21c8883f2bb7
nikolay.sohryakov@gmail.com <nikolay.sohryakov@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-11-07 13:41:57 +0000

    run script for JavaPy added. run options: ./run.sh [-ch path_to_ch_folder] [-nc if you don't want to compile a library] [-o "benchmark startup options"] [-o output_dir] Environment variables are configured automaticaly for f current bash session. -nc option added for PyJava run script too. By default output dir is <MarioAI folder>/bin/AmiCoBuild/

--- src/amico/python/JavaPy/run.sh
+++ src/amico/python/JavaPy/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #
-# AmiCoPyJava build script. Works for both Mac OS X and Linux operating systems.
+# AmiCoJavaPy build script. Works for both Mac OS X and Linux operating systems.
 # If you have a problem while compiling, please contact us to solve your problem.
 #
-# All usage options will be desctibed in documentation soon.
+# All usage options will be described in documentation soon.
 #
 # Author: Nikolay Sohryakov, nikolay.sohryakov@gmail.com
 #
 
 MARIO_DIR="../../../../bin"
 OUT_DIR="../../../../bin"
-BUILD_DIR="AmiCoBuild/PyJava"
+BUILD_DIR="AmiCoBuild/JavaPy"
 MAKE_OUT_DIR="./build"
+DEFAULT_AGENT=forwardagent.ForwardAgent.py
 CMD_LINE_OPTIONS=
-AGENT=
 LIBRARY_FILE_NAME=
 COMPILE_LIBRARY="true"
 
@@ -26,8 +26,7 @@ if [ "$#" -gt 0 ]; then
 		"-ch"	) shift; MARIO_DIR="$1";;
 		"-out"	) shift; OUT_DIR="$1";;
 		"-o"	) shift; CMD_LINE_OPTIONS="$1";;
-		"-agent") shift; AGENT="$1";;
-		*	) echo "Usage: [-ch path_to_ch_directory] [-out path_to_output_directory]"; exit 1;;
+		*	) echo "Usage: [-ch path_to_ch_directory] [-out path_to_output_directory] [-o cmd_line_options]"; exit 1;;
 		esac
 		shift
 	done
@@ -39,36 +38,44 @@ fi
 
 mkdir -p "$OUT_DIR/$BUILD_DIR"
 
+echo "$OUT_DIR/$BUILD_DIR"
+
 if test "${OS}" = ""; then
 	OS=`uname -s` # Retrieve the Operating System
 fi
 if [ "${OS}" = "Linux" ]; then
-	LIBRARY_FILE_NAME="libAmiCoPyJava.so"
+	LIBRARY_FILE_NAME="libAmiCoJavaPy.so"
 elif [ "${OS}" = "Darwin" ]; then
-	LIBRARY_FILE_NAME="libAmiCoPyJava.dylib"
+	LIBRARY_FILE_NAME="libAmiCoJavaPy.dylib"
 fi
-
+	
 if [ "$LIBRARY_FILE_NAME" = "" ]; then
 	echo "Unknown OS type: ${OS}. Exit..."
 	exit 1
 fi
-
+	
 if [ "$COMPILE_LIBRARY" = "true" ]; then
 	mv "$MAKE_OUT_DIR/$LIBRARY_FILE_NAME" "$OUT_DIR/$BUILD_DIR/"
 	rm -rf "$MAKE_OUT_DIR"
 fi
 
-cp AmiCoRunner.sh "$OUT_DIR/$BUILD_DIR/"
 cp ../agents/*.py "$OUT_DIR/$BUILD_DIR/"
 cp -r "$MARIO_DIR/ch" "$OUT_DIR/$BUILD_DIR/"
 
-if [ "$AGENT" = "" ]; then
-	echo "Incorrect input! Option '-agent <agent_file>' is necessary!"
-fi
-
 cd "$OUT_DIR/$BUILD_DIR/"
 if [ "${OS}" = "Linux" ]; then
-	./AmiCoRunner.sh "$AGENT" "$CMD_LINE_OPTIONS"
+	export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
+	echo $LD_LIBRARY_PATH
 elif [ "${OS}" = "Darwin" ]; then
-	python "$AGENT" "$CMD_LINE_OPTIONS"
+	export DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH
+	echo $LDYD_LIBRARY_PATH
+fi
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+echo $PYTHONPATH
+
+if [ "$CMD_LINE_OPTIONS" = "" ]; then
+	java ch.idsia.scenarios.Main -ag $DEFAULT_AGENT
+else
+	java ch.idsia.scenarios.Main $CMD_LINE_OPTIONS
 fi
