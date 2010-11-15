b5dd6ead82aadc226ae5787a917a4490e0c7fc04
Nikolay.Sohryakov@gmail.com <Nikolay.Sohryakov@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-11-15 22:32:39 +0000

    unit tests fix

--- src/ch/idsia/unittests/MarioEnvironmentTest.java
+++ src/ch/idsia/unittests/MarioEnvironmentTest.java
@@ -241,7 +241,7 @@ public void testMarioCenterPos() throws Exception
 
     int[] pos = env.getMarioEgoPos();
     assertEquals(2, pos[0]);
-    assertEquals(3, pos[1]);
+    assertEquals(2, pos[1]);
 }
 
 @Test
