e3ce4fa106b3a1c04a03e100b4c406ee1f734d40
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-12-19 21:20:02 +0000

    eval scripts

--- trunk/target/scripts/learning-track-evaluation.sh
+++ trunk/target/scripts/learning-track-evaluation.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+
+java -classpath .:../lib/jdom.jar ch.idsia.scenarios.champ.LearningTrack -ag competition.gic2010.learning.sergeykarakovskiy.SergeyKarakovskiy_MLPAgent -rec SergeyKarakovskiy_MLPAgent
