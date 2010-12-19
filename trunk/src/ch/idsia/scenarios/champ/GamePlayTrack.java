375f12c5e3c4cb757c4e916eb42da83835f26d96
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-12-19 22:55:55 +0000

    eval scripts

--- trunk/src/ch/idsia/scenarios/champ/GamePlayTrack.java
+++ trunk/src/ch/idsia/scenarios/champ/GamePlayTrack.java
@@ -4,14 +4,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Mario AI nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ *  Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *  Neither the name of the Mario AI nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -28,7 +28,6 @@
 package ch.idsia.scenarios.champ;
 
 import ch.idsia.agents.Agent;
-import ch.idsia.agents.AgentsPool;
 import ch.idsia.benchmark.tasks.GamePlayTask;
 import ch.idsia.benchmark.tasks.Task;
 import ch.idsia.tools.MarioAIOptions;
@@ -67,13 +66,6 @@ public static void evaluateAgent(final Agent agent)
 public static void evaluateSubmissionZip(final String zipFileName)
 {
 
-}
-
-public static void evaluateAllAgents()
-{
-//       int startingSeed = marioAIOptions.getLevelRandSeed();
-    for (Agent agent : AgentsPool.getAgentsCollection())
-        evaluateAgent(agent);
 }
 
 
--- trunk/target/scripts/learning-track-evaluation.sh
+++ trunk/target/scripts/learning-track-evaluation.sh
@@ -2,3 +2,10 @@
 
 cd ../../bin
 java -classpath .:../lib/jdom.jar ch.idsia.scenarios.champ.LearningTrack -ag competition.gic2010.learning.sergeykarakovskiy.SergeyKarakovskiy_MLPAgent -rec SergeyKarakovskiy_MLPAgent
+
+-lco off -lb on -le off -lhb off -lg on -ltb on -lhs off -lc on -lde on -ld 5 -ls 33829
+-lde on -i on -ld 30 -ls 33434
+-lde on -i on -ld 30 -ls 33434 -lhb on
+
+-lla on -le off -lhs on -lde on -ld 5 -ls 332656
+-le off -lhs on -lde on -ld 5 -ls 332656 -rec superLevel
