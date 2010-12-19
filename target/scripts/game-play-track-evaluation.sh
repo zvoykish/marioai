1099e4314e4d8dfba272cfc059844ab84b4eb661
Sergey.Karakovskiy@gmail.com <Sergey.Karakovskiy@gmail.com@91117294-33d9-11de-9c63-4752e237b823>
2010-12-19 21:21:02 +0000

    eval scripts

--- target/scripts/game-play-track-evaluation.sh
+++ target/scripts/game-play-track-evaluation.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+cd ../../bin
 java ch.idsia.scenarios.champ.GamePlayTrack -ag competition.gic2010.gameplay.sergeykarakovskiy.SergeyKarakovskiy_ForwardAgent -rec SergeyKarakovskiy_ForwardAgent
